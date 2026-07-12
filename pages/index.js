cd81625c3df471209da7857a13f8ac532a8ea6d1
Eduardo Rosa <eduardorosa.contato@gmail.com>
2026-07-12 20:52:06 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home(){
-    return <h1>Yasmim, eu amo você. se você me ama dá uma risadinha</h1>
+    return(
+    <main>
+        <h1>Yasmim, eu amo você. se você me ama dá uma risadinha 😎</h1>
+    </main>
 
+    )
 }
 
 export default Home;
